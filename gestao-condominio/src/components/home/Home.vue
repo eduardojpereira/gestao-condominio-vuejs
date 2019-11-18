aa32e1163c35622d7b9181f221e18816abd24854
Bilckz <pedrobilckjr@gmail.com>
2019-11-18 20:12:49 -0300

    adicionado grafico a tela inicial

--- gestao-condominio/src/components/home/Home.vue
+++ gestao-condominio/src/components/home/Home.vue
@@ -1,10 +1,36 @@
 <template>
-    <h1>aqui vai ter uns graficos</h1>
+  <div>
+    <div id="app">
+      <GChart type="ColumnChart" :data="chartData" :options="chartOptions" />
+    </div>
+  </div>
 </template>
 <script>
+import { GChart } from "vue-google-charts";
 export default {
-    
-}
+  name: "App",
+  components: {
+    GChart
+  },
+  data() {
+    return {
+      // Array will be automatically processed with visualization.arrayToDataTable function
+      chartData: [
+        ["Year", "Sales", "Expenses", "Profit"],
+        ["2014", 1500, 400, 200],
+        ["2015", 1170, 460, 250],
+        ["2016", 660, 1120, 300],
+        ["2017", 1030, 540, 350]
+      ],
+      chartOptions: {
+        chart: {
+          title: "Company Performance",
+          subtitle: "Sales, Expenses, and Profit: 2014-2017"
+        }
+      }
+    };
+  }
+};
 </script>
 <style>
 </style>
