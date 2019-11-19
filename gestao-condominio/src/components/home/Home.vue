29e7d8de4985289e006ef357927a8073ec4d6934
Bilckz <pedrobilckjr@gmail.com>
2019-11-18 21:29:41 -0300

    adicionadas novas formulas e alterados os graficos

--- gestao-condominio/src/components/home/Home.vue
+++ gestao-condominio/src/components/home/Home.vue
@@ -1,7 +1,27 @@
 <template>
   <div>
     <div id="app">
-      <GChart type="ColumnChart" :data="chartData" :options="chartOptions" />
+      <h1 class="title">Graficos de consumo mensais</h1>
+      <GChart type="ColumnChart" :data="chartData" :options="chartOptions" ti />
+    </div>
+    <div>
+      <div>
+        <div class="text-center d-flex pb-4">
+          <v-btn @click="all">Expandir todos</v-btn>
+          <v-btn @click="none">Comprimir todos</v-btn>
+        </div>
+        <v-row justify="center">
+          <v-expansion-panels v-model="panel" multiple>
+            <v-expansion-panel v-for="(item,i) in estrela" :key="i">
+              <v-expansion-panel-header>
+                <v-col>{{estrela[i].status}}</v-col>
+                <v-col>{{estrela[i].name}}</v-col>
+              </v-expansion-panel-header>
+              <v-expansion-panel-content>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</v-expansion-panel-content>
+            </v-expansion-panel>
+          </v-expansion-panels>
+        </v-row>
+      </div>
     </div>
   </div>
 </template>
@@ -12,25 +32,47 @@ export default {
   components: {
     GChart
   },
+        methods: {
+        brilha(index) {
+          this.estrela[index].favorite = !this.estrela[index].favorite;
+        },
+        all() {
+          this.panel = [...Array(this.items).keys()].map((k, i) => i);
+        },
+        // Reset the panel
+        none() {
+          this.panel = [];
+        }
+      },
   data() {
     return {
       // Array will be automatically processed with visualization.arrayToDataTable function
       chartData: [
-        ["Year", "Sales", "Expenses", "Profit"],
-        ["2014", 1500, 400, 200],
-        ["2015", 1170, 460, 250],
-        ["2016", 660, 1120, 300],
-        ["2017", 1030, 540, 350]
+        ["Mês", "Gasto"],
+        ["Outubro", 123.90],
+        ["Setembro", 98.76],
+        ["Agosto", 110.10]
       ],
       chartOptions: {
         chart: {
           title: "Company Performance",
-          subtitle: "Sales, Expenses, and Profit: 2014-2017"
+          subtitle: "Sales"
         }
-      }
-    };
+      },
+      estrela: [
+        { favorite: false, status: "Outubro", name: "R$ 123.90" },
+        { favorite: false, status: "Setembro", name: "R$ 98.76" },
+        { favorite: false, status: "Agosto", name: "R$ 110.10" }
+      ],
+      panel: [],
+      items: 5,
+  }
   }
 };
 </script>
 <style>
+.title {
+  margin: auto;
+  text-align: center;
+}
 </style>
